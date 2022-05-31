3cf1c2f087318e1e3fddd2e679837061eb81b02f
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-05-31 14:02:08 +0200

    Update streamlit_app.py
    substitute st.write with st.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 
-st.write('Omega 3 and Bluberry Oatmeal')
-st.write('Kale, Spinach and Rocket Smoothie')
-st.write('Hard-Boiled Free-Range Egg')
+st.text('Omega 3 and Bluberry Oatmeal') #you can probably use also st.write()
+st.text('Kale, Spinach and Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
