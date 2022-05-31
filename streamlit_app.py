cd8ecd67e443764c1b1010088ae0786dd8e59770
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-05-31 15:34:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.title('My Parents New Healthy Diner')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so the users can pick the fruit they want to include
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
