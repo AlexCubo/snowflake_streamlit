e58868e6251457e31bec29ff22127edf202d2e0e
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-05-31 14:43:05 +0200

    Update streamlit_app.py
    Add some emoji from the emoji code python

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 
-st.text('Omega 3 and Bluberry Oatmeal') #you can probably use also st.write()
-st.text('Kale, Spinach and Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('\U1F963', 'Omega 3 and Bluberry Oatmeal') #you can probably use also st.write()
+st.text('\U1F957','Kale, Spinach and Rocket Smoothie')
+st.text('\U1F414', 'Hard-Boiled Free-Range Egg')
+st.text('\U1F951', '\U1F35E', 'Avocado Toast')
+st.text('\U1F347', '\U1F350', '\U1F352', 'Some fruit')
