22ab034e9890cd65442b56ad3bc5f133abf8ac52
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-06-10 09:57:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,9 @@ st.header("The FRUIT_LOAD_LIST table contains many fruits.")
 all_fruits = my_cur.fetchall()
 st.dataframe(all_fruits)
 
+fruit_choice = st.text_input('What fruit would you like to add?')
+st.write('Thanks for adding', fruit_choice)
+
 
 
 
