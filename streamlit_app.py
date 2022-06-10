9c44183d629a459aef478661ad4d05ffd5848e3d
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-06-10 08:51:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,11 +37,11 @@ st.dataframe(fruityvice_normalized)
 
 # Let's query our Snowflake Trial Account Metadata
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#st.text("Hello from Snowflake:")
-#st.text(my_data_row)
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 
 
