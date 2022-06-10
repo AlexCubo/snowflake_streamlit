265186b0e53b3b031b6e8978e48b95f2ea83393e
Alessandro <76247689+AlexCubo@users.noreply.github.com>
2022-06-10 10:15:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,6 @@ st.title('My Mom\'s New Healthy Diner')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Let's put a pick list here so the users can pick the fruit they want to include
-fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruit_to_show = my_fruit_list.loc[fruit_selected]
-
 st.header('Breakfast Menu')
 
 st.text("\U0001F963 Omega 3 and Bluberry Oatmeal") #you can probably use also st.write()
@@ -20,6 +16,11 @@ st.text("\U0001F951 \U0001F35E Avocado Toast")
 st.text("\U0001F347 \U0001F350 \U0001F352 Some fruit")
 
 st.header("\U0001F34C \U0001F353 Build Your Own Fruit Smoothie \U0001F95D \U0001F347")
+
+#Let's put a pick list here so the users can pick the fruit they want to include
+fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruit_selected]
+
 #Display the table on the page
 st.dataframe(fruit_to_show)
 
